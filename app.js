e2454a264ef8ce9cf4037639e6f3148d29517d16
michaelmichaelshvili <michaelm748@gmail.com>
2019-05-19 20:01:58 +0300

    add moduls

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-asdas
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hellosas");
+});
+
+const port = process.env.PORT || 3000; //environment variable
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
