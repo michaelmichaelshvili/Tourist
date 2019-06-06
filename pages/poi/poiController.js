119b5b672f9658981f8ead874b955c3438bb6818
michaelmichaelshvili <michaelm748@gmail.com>
2019-06-06 11:03:25 +0300

    start html

--- pages/poi/poiController.js
+++ pages/poi/poiController.js
@@ -1,5 +1,5 @@
-let app = angular.module('myApp');
-
+// let app = angular.module('myApp');
+this.poi_name = "Jerusalem";
 app.controller('poiController',function($scope,$routeParams){
     console.log($routeParams.poi_name);
 });
